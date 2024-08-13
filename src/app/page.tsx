83f5291cbc38ff27b6678460e624d6a2a81a1a8e
Ahmed Drioueche <adsrahmed@gmail.com>
2024-08-13 15:27:37 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import ProjectsCarousel from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Banner from "./components/Banner"
+import SimpleBanner from "./components/SimpleBanner"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      <Banner />
+      <SimpleBanner />
       <ProjectsCarousel/>
       <Skills/>
       <Contact/>
