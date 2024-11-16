5087250690496f50497923f83a4d7b0ecd4391de
Ahmed Drioueche <adsrahmed@gmail.com>
2024-11-16 15:47:05 +0100

    updated navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from "react";
 import { animateScroll } from "react-scroll";
 import { Spotlight } from "./ui/spotlight";
-import Image from 'next/image'; 
-import { useRouter } from 'next/navigation';
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const MyNavBar: React.FC = () => {
   const [activeLink, setActiveLink] = useState<string>("home");
@@ -17,8 +17,8 @@ const MyNavBar: React.FC = () => {
 
     handleResize();
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
@@ -32,8 +32,8 @@ const MyNavBar: React.FC = () => {
 
   const handleNavClick = (section: string) => {
     setActiveLink(section);
-   
-    setIsOpen(false); 
+
+    setIsOpen(false);
   };
 
   const toggleMenu = () => {
@@ -43,23 +43,29 @@ const MyNavBar: React.FC = () => {
   const handleLogoClick = () => {
     router.push("/");
     animateScroll.scrollToTop();
-  }
+  };
 
   return (
     <nav
       className={`w-full fixed top-0 left-0 z-50 bg-gray-900 shadow-md py-4 transition-all duration-300 font-stix`}
     >
-      {windowWidth > 768 ?      
-       <Spotlight className="absolute top-[20%] right-[-5%] w-[1200px] h-[1300px]" fill="white" />
-        : 
-       <Spotlight className="absolute top-[20%] right-[-180%] w-[1100px] h-[1000px]" fill="white" />
-      }
+      {windowWidth > 768 ? (
+        <Spotlight
+          className="absolute top-[20%] right-[-5%] w-[1200px] h-[1300px]"
+          fill="white"
+        />
+      ) : (
+        <Spotlight
+          className="absolute top-[20%] right-[-180%] w-[1100px] h-[1000px]"
+          fill="white"
+        />
+      )}
       <div className="container mx-auto flex items-center justify-between px-4">
         <div
-          className="text-white font-bold text-2xl cursor-pointer"
+          className="text-white font-bold text-xl cursor-pointer"
           onClick={() => handleLogoClick()}
         >
-          Portfolio
+          My Portfolio
         </div>
         {/* Large Screens: Regular Navbar */}
         <div className="hidden md:flex space-x-6">
@@ -69,12 +75,38 @@ const MyNavBar: React.FC = () => {
             onClick={() => handleNavClick("home")}
           >
             <span
-              className={`relative transition-colors duration-300 ${activeLink === "home" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+              className={`relative transition-colors duration-300 ${
+                activeLink === "home"
+                  ? "text-red-500"
+                  : "text-white group-hover:text-red-500"
+              }`}
             >
               Home
             </span>
             <span
-              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "home" ? "w-full" : "group-hover:w-full"}`}
+              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                activeLink === "home" ? "w-full" : "group-hover:w-full"
+              }`}
+            ></span>
+          </a>
+          <a
+            href="/#experience"
+            className="relative cursor-pointer text-sm sm:text-lg font-medium group"
+            onClick={() => handleNavClick("experience")}
+          >
+            <span
+              className={`relative transition-colors duration-300 ${
+                activeLink === "experience"
+                  ? "text-red-500"
+                  : "text-white group-hover:text-red-500"
+              }`}
+            >
+              Experience
+            </span>
+            <span
+              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                activeLink === "experience" ? "w-full" : "group-hover:w-full"
+              }`}
             ></span>
           </a>
 
@@ -84,12 +116,18 @@ const MyNavBar: React.FC = () => {
             onClick={() => handleNavClick("projects")}
           >
             <span
-              className={`relative transition-colors duration-300 ${activeLink === "projects" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+              className={`relative transition-colors duration-300 ${
+                activeLink === "projects"
+                  ? "text-red-500"
+                  : "text-white group-hover:text-red-500"
+              }`}
             >
               Projects
             </span>
             <span
-              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "projects" ? "w-full" : "group-hover:w-full"}`}
+              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                activeLink === "projects" ? "w-full" : "group-hover:w-full"
+              }`}
             ></span>
           </a>
 
@@ -99,12 +137,38 @@ const MyNavBar: React.FC = () => {
             onClick={() => handleNavClick("skills")}
           >
             <span
-              className={`relative transition-colors duration-300 ${activeLink === "skills" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+              className={`relative transition-colors duration-300 ${
+                activeLink === "skills"
+                  ? "text-red-500"
+                  : "text-white group-hover:text-red-500"
+              }`}
             >
               Skills
             </span>
             <span
-              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "skills" ? "w-full" : "group-hover:w-full"}`}
+              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                activeLink === "skills" ? "w-full" : "group-hover:w-full"
+              }`}
+            ></span>
+          </a>
+          <a
+            href="/#contact"
+            className="relative cursor-pointer text-sm sm:text-lg font-medium group"
+            onClick={() => handleNavClick("contact")}
+          >
+            <span
+              className={`relative transition-colors duration-300 ${
+                activeLink === "contact"
+                  ? "text-red-500"
+                  : "text-white group-hover:text-red-500"
+              }`}
+            >
+              Contact
+            </span>
+            <span
+              className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                activeLink === "contact" ? "w-full" : "group-hover:w-full"
+              }`}
             ></span>
           </a>
         </div>
@@ -174,23 +238,60 @@ const MyNavBar: React.FC = () => {
             </svg>
           </button>
         </div>
-
         <div
-          className={`md:hidden w-full ${isOpen ? "block" : "hidden"} absolute top-full left-0 bg-gradient-to-b from-gray-600 to-gray-900 py-4 z-50`}
-  >
-          <div className="flex flex-col items-start space-y-4 px-4">
+          className={`md:hidden w-full ${
+            isOpen ? "block" : "hidden"
+          } fixed top-0 left-0 bg-gray-900 py-4 z-50 h-full`}
+        >
+          <div className="absolute top-4 right-4">
+            {/* Close Button */}
+            <button
+              onClick={toggleMenu}
+              className="text-white text-2xl focus:outline-none"
+            >
+              &times; {/* This is the close icon (X) */}
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center space-y-4 h-full">
             <a
               href="/#home"
               className="relative cursor-pointer text-lg font-medium group"
               onClick={() => handleNavClick("home")}
             >
               <span
-                className={`relative transition-colors duration-300 ${activeLink === "home" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+                className={`relative transition-colors duration-300 ${
+                  activeLink === "home"
+                    ? "text-red-500"
+                    : "text-white group-hover:text-red-500"
+                }`}
               >
                 Home
               </span>
               <span
-                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "home" ? "w-full" : "group-hover:w-full"}`}
+                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                  activeLink === "home" ? "w-full" : "group-hover:w-full"
+                }`}
+              ></span>
+            </a>
+
+            <a
+              href="/#experience"
+              className="relative cursor-pointer text-lg font-medium group"
+              onClick={() => handleNavClick("experience")}
+            >
+              <span
+                className={`relative transition-colors duration-300 ${
+                  activeLink === "experience"
+                    ? "text-red-500"
+                    : "text-white group-hover:text-red-500"
+                }`}
+              >
+                Experience
+              </span>
+              <span
+                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                  activeLink === "experience" ? "w-full" : "group-hover:w-full"
+                }`}
               ></span>
             </a>
 
@@ -200,12 +301,18 @@ const MyNavBar: React.FC = () => {
               onClick={() => handleNavClick("projects")}
             >
               <span
-                className={`relative transition-colors duration-300 ${activeLink === "projects" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+                className={`relative transition-colors duration-300 ${
+                  activeLink === "projects"
+                    ? "text-red-500"
+                    : "text-white group-hover:text-red-500"
+                }`}
               >
                 Projects
               </span>
               <span
-                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "projects" ? "w-full" : "group-hover:w-full"}`}
+                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                  activeLink === "projects" ? "w-full" : "group-hover:w-full"
+                }`}
               ></span>
             </a>
 
@@ -215,42 +322,69 @@ const MyNavBar: React.FC = () => {
               onClick={() => handleNavClick("skills")}
             >
               <span
-                className={`relative transition-colors duration-300 ${activeLink === "skills" ? "text-red-500" : "text-white group-hover:text-red-500"}`}
+                className={`relative transition-colors duration-300 ${
+                  activeLink === "skills"
+                    ? "text-red-500"
+                    : "text-white group-hover:text-red-500"
+                }`}
               >
                 Skills
               </span>
               <span
-                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${activeLink === "skills" ? "w-full" : "group-hover:w-full"}`}
+                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                  activeLink === "skills" ? "w-full" : "group-hover:w-full"
+                }`}
               ></span>
             </a>
-            <div className="flex space-x-4">
+            <a
+              href="/#contact"
+              className="relative cursor-pointer text-lg font-medium group"
+              onClick={() => handleNavClick("contact")}
+            >
+              <span
+                className={`relative transition-colors duration-300 ${
+                  activeLink === "contact"
+                    ? "text-red-500"
+                    : "text-white group-hover:text-red-500"
+                }`}
+              >
+                Contact
+              </span>
+              <span
+                className={`block h-[2px] w-0 bg-red-500 absolute left-0 bottom-[-2px] transition-all duration-300 ${
+                  activeLink === "contact" ? "w-full" : "group-hover:w-full"
+                }`}
+              ></span>
+            </a>
+
+            <div className="flex space-x-4 mt-6">
               <a href="https://linkedin.com/in/ahmed-drioueche-aa02732b7">
-              <Image
-                src="/nav-icon1.svg"
-                alt="icon1"
-                width={24}
-                height={24}
-                className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-              />
-            </a>
-            <a href="https://www.facebook.com/ahmed.69.420">
-              <Image
-                src="/nav-icon2.svg"
-                alt="icon2"
-                width={24}
-                height={24}
-                className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-              />
-            </a>
-            <a href="https://www.instagram.com/ahmed.driouche.420">
-              <Image
-                src="/nav-icon3.svg"
-                alt="icon3"
-                width={24}
-                height={24}
-                className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-              />
-            </a>
+                <Image
+                  src="/nav-icon1.svg"
+                  alt="icon1"
+                  width={24}
+                  height={24}
+                  className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                />
+              </a>
+              <a href="https://www.facebook.com/ahmed.69.420">
+                <Image
+                  src="/nav-icon2.svg"
+                  alt="icon2"
+                  width={24}
+                  height={24}
+                  className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                />
+              </a>
+              <a href="https://www.instagram.com/ahmed.driouche.420">
+                <Image
+                  src="/nav-icon3.svg"
+                  alt="icon3"
+                  width={24}
+                  height={24}
+                  className="transition-transform duration-300 ease-in-out transform hover:scale-110"
+                />
+              </a>
             </div>
           </div>
         </div>
